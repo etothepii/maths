dc086755b17c5bc0c685545a7c38b65e696267b9
James Robinson <james@bermuda.earth>
2020-03-30 20:58:26 -0300

    Added test for odd, odd

--- collatz/test_code.py
+++ collatz/test_code.py
@@ -13,3 +13,10 @@ def test_can_step_2():
     expected = {Number(6, 1, 5)}
     result = start.step()
     assert expected == result
+
+
+def test_can_step_3():
+    start = Number(9, 1, 8)
+    expected = {Number(9, 2, 4), Number(27, 1, 25)}
+    result = start.step()
+    assert expected == result
